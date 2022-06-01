80bd43d7fefcc0ffbadaca6b46a2a465e843996e
manojbarkicodee <manojjunjappabarki@gmail.com>
2022-06-01 18:04:45 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import styles from "./sidebar.module.css"
 
+import {
+  
+  Box,
+  Text
+} from '@chakra-ui/react'
 function App() {
+  let data=[
+    {icon:"fa-solid fa-house",showdata:"Home"},
+{icon:"fa-solid fa-location-arrow",showdata:"Treanding"},
+
+{icon:"fa-solid fa-compass",showdata:"Explore"},
+{icon:"fa-solid fa-star",showdata:"Favorities"},
+{icon:"fa-solid fa-gear",showdata:"settings"},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Box className={styles.main} >
+<Text className={styles.text} >Logo</Text>
+{data.map((el)=>(
+  <Box className={styles.box}  >
+  <i className={el.icon}></i>
+  <Text fontSize="2xl">{el.showdata}</Text>
+  </Box>
+))}
+
+
+  </Box>
+
     </div>
   );
 }
